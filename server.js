54e9d5a192e4c36d5a962c64b12c17643e67d5f7
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-02-27 17:03:29 -0700

    code that takes in notes

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+// Requiring dependencies
 const express = require('express');
-
 const fs = require('fs');
 const path = require('path')
+
+//Set up Express app
 const app = express();
 const PORT = process.env.PORT || 3002; 
 
-// Middleware
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+//Routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
@@ -18,7 +21,27 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-// Leave this at the bottom
+//Takes in the notes and saves to db.json.
+app.post('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, '/db/db.json'), (error, response) => {
+    if (error) throw (error);
+    let notes = JSON.parse(response);
+    let noteReq = req.body;
+    let newNoteId = notes.length + 1;
+    let newNote = {
+      id: newNoteId,
+      title: noteReq.title,
+      text: noteReq.text,
+    };
+    notes.push(newNote);
+    res.json(newNote);
+    fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes, null, 2), (error) => {
+      if (error) throw (error);
+    });
+  });
+});
+
+// Leave this at the bottom. Server listening.
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
