1c4f19116bbdb70dbb5e0e4c9defc4be86630404
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-02-28 13:51:58 -0700

    adding delete option

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-//Set up Express app
+// Set up Express app
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-//Middleware
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-//Routes
+// Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -25,6 +25,7 @@ app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
+// Adding a new note
 app.post("/api/notes", (req, res) => {
   fs.readFile(path.join(__dirname, "/db/db.json"), (error, response) => {
     if (error) throw (error);
@@ -44,7 +45,20 @@ app.post("/api/notes", (req, res) => {
   });
 });
 
-// Leave this at the bottom. Server listening.
+// Deleting a note
+app.delete("/api/notes/:noteId", (req, res) => {
+  fs.readFile(path.join(__dirname, "/db/db.json"), (err, response) => {
+    if (err) throw (err);
+    let myArray = JSON.parse(data)
+    let newArray = myArray.filter (note => note.id !== req.params.noteId)
+    console.log(newArray)
+    fs.writeFile(path.join(__dirname, "/db/db.json"), newArray, (err) => {
+      if (err) throw (err);
+    })
+  })
+})
+
+//Leave this at the bottom. Server listening.
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
