4ae2b3cff644907cdbd89915197deb634e5c9f44
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-02-27 18:30:00 -0700

    Reverted to old code

--- server.js
+++ server.js
@@ -1,48 +1,45 @@
 // Requiring dependencies
-const express = require('express');
-const fs = require('fs');
-const path = require('path')
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 //Set up Express app
 const app = express();
-const PORT = process.env.PORT || 3002; 
+const PORT = process.env.PORT || 3002;
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/db/db.json'));
-}) 
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
 
-//Takes in the notes and saves to db.json.
-app.post('/api/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, '/db/db.json'), (error, response) => {
-    if (error) {
-      console.log(error);
-    }
+app.post("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "/db/db.json"), (error, response) => {
+    if (error) throw (error);
     let notes = JSON.parse(response);
     let noteReq = req.body;
     let newNoteId = notes.length + 1;
     let newNote = {
       id: newNoteId,
       title: noteReq.title,
-      text: noteReq.text
+      text: noteReq.text,
     };
     notes.push(newNote);
     res.json(newNote);
-    fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes, null, 2), (error) => {
-      if (error) throw (error);
+    fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notes, null, 2), (err) => {
+      if (err) throw (err);
     });
   });
 });
